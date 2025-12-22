a799a4920c58b4b900e96bb427746d4f2df69dc6
Yonanda Haryono <81000399+yonanda123@users.noreply.github.com>
2025-12-22 15:57:22 +0700

    add middleware sso

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,18 +2,57 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Validation\UnauthorizedException;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckSsoToken;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'sso' => CheckSsoToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ValidationException $exception, $request) {
+            return response()->json([
+                'code'    => 422,
+                'message' => 'Validation Error',
+                'errors'    => $exception->validator->errors(),
+            ], 422);
+        });
+
+        $exceptions->render(function (Throwable $exception, $request) {
+            // Route tidak ditemukan
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'code'    => 403,
+                    'message' => 'Forbidden: route tidak ditemukan.',
+                    'data'    => null,
+                ], 403);
+            }
+
+            // Tidak punya hak akses / permission
+            if ($exception instanceof UnauthorizedException) {
+                return response()->json([
+                    'code'    => 403,
+                    'message' => 'Forbidden: Anda tidak memiliki izin untuk mengakses route ini.',
+                    'data'    => null,
+                ], 403);
+            }
+
+            // Default: error lainnya
+            return response()->json([
+                'code'    => 500,
+                'message' => 'Internal Server Error',
+                'data'    => null
+            ], 500);
+        });
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeritaController;
 use App\Http\Controllers\TestimoniController;
@@ -10,18 +9,20 @@ use App\Http\Controllers\TentangkamiController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
+Route::middleware(['sso:1'])->group(function () {
+    Route::apiResource('berita', BeritaController::class);
+    Route::post('/berita/{id}/update', [BeritaController::class, 'update']);
+    Route::patch('berita/{id}/status', [BeritaController::class, 'updateStatus']);
+    Route::apiResource('testimoni', TestimoniController::class);
+    Route::apiResource('galeri', GaleriController::class);
+    Route::post('/galeri/{id}/update', [GaleriController::class, 'update']);
+    Route::apiResource('tentang-kami', TentangkamiController::class);
+    Route::apiResource('program', ProgramController::class);
+    Route::post('/program/{id}/update', [ProgramController::class, 'update']);
+    Route::get('/dashboard/count', [DashboardController::class, 'count']);
+});
 
-Route::apiResource('berita', BeritaController::class);
-Route::post('/berita/{id}/update', [BeritaController::class, 'update']);
-Route::patch('berita/{id}/status', [BeritaController::class, 'updateStatus']);
-Route::apiResource('testimoni', TestimoniController::class);
-Route::apiResource('galeri', GaleriController::class);
-Route::post('/galeri/{id}/update', [GaleriController::class, 'update']);
-Route::apiResource('tentang-kami', TentangkamiController::class);
-Route::apiResource('program', ProgramController::class);
-Route::post('/program/{id}/update', [ProgramController::class, 'update']);
-Route::get('/dashboard/count', [DashboardController::class, 'count']);
-
-
-Route::get('/sso/callback', [AuthController::class, 'callback']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::prefix('sso')->group(function () {
+    Route::get('/callback', [AuthController::class, 'callback']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
